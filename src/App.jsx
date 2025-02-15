dc350f509225cf9358acfe110c4dd4787672a67b
patitapaban <patitapabanpanda046@gmail.com>
2025-02-15 16:43:29 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-// UseState
-import React, { useState } from "react";
+// useEffect
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+  useEffect(() => {
+    document.title = `${count} new Messages!`;
+  }, []);
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const Reset = () => {
-    setCount(0);
-  };
   return (
     <div>
-      <h1>Counter:{count}</h1>
-      <button onClick={increment}>Increase</button>
-      <button onClick={Reset}>Reset</button>
-      <button onClick={decrement}>Decrease</button>
+      <h3> {count} New Message!</h3>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
   );
 };
